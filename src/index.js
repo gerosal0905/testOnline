611eea2ba99c39d910da268f64791a8497907a52
gerosal0905 <111438534+gerosal0905@users.noreply.github.com>
2022-11-05 18:42:41 +0800

    BrowserRouter again

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './Login';
 import './index.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
